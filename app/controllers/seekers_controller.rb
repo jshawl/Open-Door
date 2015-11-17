62f343a55e5a7c64a23ba958e68b7b8b34f43945
Markham Shofner <markham.shofner@gmail.com>
2015-11-17 16:50:20 -0500

    preliminary version of RESTful routes for seekers.

--- app/controllers/seekers_controller.rb
+++ app/controllers/seekers_controller.rb
@@ -3,5 +3,45 @@ class SeekersController < ApplicationController
     @seekers=Seeker.all
   end
 
-  
+  def show
+    @seeker=Seeker.find(params[:id])
+  end
+
+  def new
+    @seeker=Seeker.new
+  end
+
+  def edit
+    @seeker=Seeker.find(params[:id])
+  end
+
+  def create
+    @seeker = Seeker.new(seeker_params)
+    if @seeker.save
+      redirect_to @seeker
+    else
+      render "new"
+    end
+  end
+
+  def update
+    @seeker = Seeker.find(params[:id])
+    if @seeker.update(seeker_params)
+      redirect_to @seeker
+    else
+      render "edit"
+    end
+  end
+
+  def destroy
+    @seeker = Seeker.find(params[:id])
+    @seeker.destroy
+    redirect_to seekers_path
+  end
+
+  private
+    def seeker_params
+      params.require(:seeker).permit(:first_name, :last_name, :bio)
+    end
+
 end
