178562e4606aee64be98462c2cdb83e70f024e47
Markham Shofner <markham.shofner@gmail.com>
2015-11-18 12:11:17 -0500

    add in seed data generator for seekers, using a gem called 'forgery' that helps generate fake data

--- Gemfile
+++ Gemfile
@@ -1,5 +1,6 @@
 source 'https://rubygems.org'
 
+gem 'forgery'
 gem 'devise'
 gem 'pry'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
--- db/seeds.rb
+++ db/seeds.rb
@@ -7,4 +7,26 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
 
-seekers = Seeker.create!([{first_name: "Grobase", last_name: "Yabase"}])
+seekers = []
+20.times do |i|
+  seekers.push(Seeker.create!({first_name: Forgery('name').first_name,
+                last_name: Forgery('name').last_name,
+                gender: Forgery('personal').abbreviated_gender,
+                photo_url: Forgery('internet').domain_name,
+                city: Forgery('address').city,
+                email: Forgery('internet').email_address,
+                phone_number: Forgery('address').phone,
+                bio: Forgery('lorem_ipsum').paragraph,
+                }))
+end
+
+binding.pry
+
+# organization_name
+# photo_url
+# city
+# POC_first_name
+# POC_last_name
+# email
+# phone_number
+# bio
